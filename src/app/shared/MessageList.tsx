1defee8f19e4a74ead04380816cd5d710a9185cf
jldec <jurgen@jldec.me>
2025-08-03 19:52:49 +0100

    tweak

--- src/app/shared/MessageList.tsx
+++ src/app/shared/MessageList.tsx
@@ -19,7 +19,7 @@ function TextMessage({ message, role }: { message: string; role: Message['role']
 function JsonMessage({ message }: any) {
   return (
     <div className="text-sm border border-gray-300 rounded-2xl p-2 ml-10 whitespace-pre">
-      {JSON.stringify(message, null, 2)}
+      {typeof message === 'object' ? JSON.stringify(message, null, 2) : message}
     </div>
   )
 }
