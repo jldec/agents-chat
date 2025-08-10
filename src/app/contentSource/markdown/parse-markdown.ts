6a813f367f5c41e53eef930a75a7ba32ef29ef0f
jldec <jurgen@jldec.me>
2025-08-10 20:13:38 +0100

    remove flakey memoization in RSC workers

--- src/app/contentSource/markdown/parse-markdown.ts
+++ src/app/contentSource/markdown/parse-markdown.ts
@@ -1,11 +1,17 @@
 import markdownit from 'markdown-it'
 import { Options, imagePlugin } from './image-plugin'
+import linksPlugin from 'markdown-it-replace-link'
 
 export function parseMarkdown(s: string, options: Options = {}) {
   const md = markdownit({
     linkify: true,
     html: true
-  }).use(imagePlugin, options)
+  })
+    .use(imagePlugin, options)
+    .use(linksPlugin, {
+      // for localhost development, remove origin from "own" hrefs
+      replaceLink: (link) => link.replace('https://agents-chat.jldec.me', '')
+    })
 
   return md.render(s)
 }
