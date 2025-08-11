e527e36429f5130e3a274e388ad15bd73039fc70
Jürgen Leschner <jldec@users.noreply.github.com>
2025-08-11 22:53:25 +0100

    Remove unused image handling and unused icons (#74)

--- src/app/contentSource/markdown/parse-markdown.test.ts
+++ src/app/contentSource/markdown/parse-markdown.test.ts
@@ -4,14 +4,12 @@ import { parseMarkdown } from './parse-markdown'
 const markdown = `# markdown header
 paragraph 1
 
-paragraph 2
-![alt text](https://example.com/image.png)
+[on-site-anchor](https://agents-chat.jldec.me/some-link)
 `
 
 const expectedResult = `<h1>markdown header</h1>
 <p>paragraph 1</p>
-<p>paragraph 2
-<img src="/img/1xzq9lwjinar8?og=https%3A%2F%2Fexample.com%2Fimage.png" alt="alt text"></p>
+<p><a href="/some-link">on-site-anchor</a></p>
 `
 
 test('parseMarkdown', {}, () => {
