2f11991504467c91561ec02ee1034c44f4402fa0
jldec <jurgen@jldec.me>
2025-10-02 14:37:54 -0400

    rm clouldflare agents sdk

--- README.md
+++ README.md
@@ -25,14 +25,10 @@ sociallinks:
 
 # Multi-user AI Chat with RedwoodSDK RSC and Cloudflare Agents
 
-This is an experimental project, looking at how to live-stream AI responses back to multiple connected clients. All implementations use Cloudflare durable objects and React Server Components (RSC) with [RedwoodSDK](https://rwsdk.com/).
+See https://github.com/jldec/agents-chat/pull/101
 
-Deployed at https://agents-chat.jldec.me/
+Removed all dependencies except content source/theme and:
 
 > 1. **[RSC Chat](https://agents-chat.jldec.me/chat-rsc)** - Uses RedwoodSDK realtime websockets
 >
-> 3. **[Agent Chat](https://agents-chat.jldec.me/chat-agent)** - Uses Cloudflare Agents websockets
->
 > 7. **[Time](https://agents-chat.jldec.me/time)** - Bonus - My very first foray into RSCs and RedwoodSDK
-
-Companion repo for blog post: [Multi-user AI chat with RedwoodSDK RSC and Cloudflare agents](https://jldec.me/blog/multi-user-ai-chat-with-redwoodsdk-rsc-and-cloudflare-agents).
--- src/worker.tsx
+++ src/worker.tsx
@@ -4,17 +4,13 @@ import { defineApp } from 'rwsdk/worker'
 import { realtimeRoute } from 'rwsdk/realtime/worker'
 import { render, route, layout } from 'rwsdk/router'
 
-import { ChatAgent } from './app/chat-agent/ChatAgent'
-import { chatAgentApiRoutes } from './app/chat-agent/api-routes'
 import { ChatRSC } from './app/chat-rsc/ChatRSC'
 import { Document } from './app/Document'
-import { routeAgents } from './lib/routeAgents'
 import { Time } from './app/time/Time'
 import { timeApiRoutes } from './app/time/api-routes'
 
 export { ChatDurableObject } from './lib/ChatStore'
 export { RealtimeDurableObject } from 'rwsdk/realtime/durableObject'
-export { WebsocketAgent } from './app/chat-agent/WebsocketAgent'
 
 import type { ContentPageContext } from './app/contentSource/types'
 import { ContentLayout } from './app/contentTheme/ContentLayout'
@@ -28,17 +24,14 @@ export type AppContext = {
 
 const app = defineApp([
   realtimeRoute(() => env.REALTIME_DURABLE_OBJECT),
-  routeAgents({ prefix: '/agents/' }),
   contentMiddleware({ ignore: '/api/' }),
   render(
     Document,
     layout(ContentLayout, [
       route('/chat-rsc', ChatRSC),
-      route('/chat-agent', ChatAgent),
       route('/time', Time)
     ])
   ),
-  ...chatAgentApiRoutes,
   ...timeApiRoutes,
   ...contentApiRoutes,
   render(Document, [route('*', contentTheme)])
